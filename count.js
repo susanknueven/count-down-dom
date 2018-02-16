1fbe1d4291df224a126dab01c6324fd36936e4ab
Knueven <susan.knueven@kroger.com>
2018-02-15 21:12:10 -0500

    validate input is number, save user default time, fix heading bug

--- count.js
+++ count.js
@@ -7,38 +7,98 @@ const startButton = document.getElementById('startButton');
 const stopButton = document.getElementById('stopButton');
 const READY_TO_COUNT = 'READY_TO_COUNT';
 const COUNTING = 'COUNTING';
+const COUNTING_DOWN_TEXT = 'Counting Down...';
+const TIMES_UP_TEXT = `TIME'S UP!`;
+const INVALID_INPUT = 'INVALID_INPUT';
 let countStatus;
 let timer;
+let defaultCount = { min: 0, sec: 3 };
+let userDefaultCount = {};
+
+function userHasDefault() {
+  return (!isNaN(userDefaultCount.min) && !isNaN(userDefaultCount.sec));
+}
+
+function validateInputAndSetHeadingText() {
+  const count = retrieveUserCount();
+  if (count === INVALID_INPUT) {
+    setDefaultCount();
+  }
+  setHeadingElementText(COUNTING_DOWN_TEXT);
+  setStartButtonDisabled(false);
+}
+
+function setStartButtonDisabled(isDisabled) {
+  startButton.disabled = isDisabled;
+}
 
 function setInputReadOnlyAttribute(readOnly) {
   minutesElement.readOnly = readOnly;
   secondsElement.readOnly = readOnly;
 }
 
+function setHeadingElementText(text) {
+  headingElement.innerHTML = text;
+}
+
+function getDefaultCount() {
+  if(userHasDefault()) {
+    setMinutes(userDefaultCount.min);
+    setSeconds(userDefaultCount.sec);
+  } else {
+    setMinutes(defaultCount.min);
+    setSeconds(defaultCount.sec);
+  }
+  setHeadingElementText(COUNTING_DOWN_TEXT);
+  setStartButtonDisabled(false);
+}
+
 function setDefaultCount() {
-  minutesElement.value = pad(0);
-  secondsElement.value = pad(3);
-  headingElement.innerHTML = 'Counting Down...';
-  startButton.disabled = false;
+  userDefaultCount = { min: getMinutes(), sec: getSeconds() };
+}
+
+function isPositiveNumber(num) {
+  return (!isNaN(num) && num > 0);
 }
 
 function retrieveUserCount() {
-  const userCountMinutes = parseInt(minutesElement.value);
-  const userCountSeconds = parseInt(secondsElement.value);
-  const countSeconds = userCountSeconds + userCountMinutes * minToSec;
-  return countSeconds;
+  const userCountMinutes = getMinutes();
+  const userCountSeconds = getSeconds();
+  const countInSeconds = userCountSeconds + userCountMinutes * minToSec;
+  if (isPositiveNumber(countInSeconds)) {
+    setMinutes(userCountMinutes);
+    setSeconds(userCountSeconds);
+    return countInSeconds;
+  }
+  return INVALID_INPUT;
 }
 
 function pad(number) {
   return number < 10 ? `0${number}` : number;
 }
 
-function getMinutes(seconds) {
-  return pad(parseInt(seconds / minToSec, 10));
+function setMinutes(minutes) {
+  minutesElement.value = pad(minutes);
 }
 
-function getSeconds(seconds) {
-  return pad(seconds % minToSec);
+function getMinutes() {
+  return parseInt(minutesElement.value);
+}
+
+function setSeconds(seconds) {
+  secondsElement.value = pad(seconds);
+}
+
+function getSeconds() {
+  return parseInt(secondsElement.value);
+}
+
+function calculateMinutes(seconds) {
+  return parseInt(seconds / minToSec, 10);
+}
+
+function calculateSeconds(seconds) {
+  return seconds % minToSec;
 }
 
 function playSound(soundElement) {
@@ -53,20 +113,22 @@ function stopTimer() {
 function countDown(countInSeconds) {
   timer = setInterval(function() {
     countInSeconds --;
-    minutesElement.value = getMinutes(countInSeconds);
-    secondsElement.value = getSeconds(countInSeconds);
-    if (countInSeconds === 0) {
+    setMinutes(calculateMinutes(countInSeconds));
+    setSeconds(calculateSeconds(countInSeconds));
+    if (countInSeconds <= 0) {
       stopTimer();
       playSound(airHornElement);
-      headingElement.innerHTML = "TIME'S UP!"
+      setHeadingElementText(TIMES_UP_TEXT);
+      setInputReadOnlyAttribute(false);
     }
   }, 1000);
 }
 
 function startCountDown() {
   const count = retrieveUserCount();
+  setDefaultCount(); 
   countDown(count);
-  startButton.disabled = true;
+  setStartButtonDisabled(true);
   setInputReadOnlyAttribute(true);
   countStatus = COUNTING;
 }
@@ -76,7 +138,7 @@ function stopCountDown() {
     const currentCount = stopTimer();
     setInputReadOnlyAttribute(false);
     if (currentCount > 0) {
-      startButton.disabled = false;
+      setStartButtonDisabled(false);
       countStatus = READY_TO_COUNT;
     }
   }
@@ -87,7 +149,7 @@ function reset() {
     stopTimer();
     setInputReadOnlyAttribute(false);
   } 
-  setDefaultCount();
+  getDefaultCount();
   countStatus = READY_TO_COUNT;
 }
 
